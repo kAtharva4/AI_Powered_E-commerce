836e04657a3d1bc1eb0f424160e763c18a32b078
Atharva Kalokhe <163155381+kAtharva4@users.noreply.github.com>
2025-09-21 21:29:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://ai-powered-e-commerce-frontendone.onrender.com" , "http://localhost:5174"],
+ origin:["https://ai-powered-e-commerce-frontendone.onrender.com" , "https://ai-powered-e-commerce-admin.onrender.com"],
  credentials:true
 }))
 
